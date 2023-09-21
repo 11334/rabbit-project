ac2151e1b067feed6856e20334ee13373a575b94
JennC <2843274115LL@gmail.com>
2023-09-21 10:07:02 +0800

    获取前台分类数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
-import { getHomeBannerAPI } from '@/services/home';
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home';
 import CustomNavbar from './componnets/CustomNavbar.vue';
 import CategoryPanel from './componnets/CategoryPanel.vue';
 import { onLoad } from '@dcloudio/uni-app';
 import { ref } from 'vue';
 import type { BannerItem } from '@/types/home';
 // 空数组在 ts 里面没有指定类型  以后只能是空数组 不能存放任何数据
+// 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   // console.log(res.result);
   bannerList.value = res.result
+}
+
+// 获取前台分类数据
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  console.log(res);
 
 }
+
 // 页面加载的时候调用一次
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
