4cad0d7e57ed54d5e828e8888bf4b56137ee19dd
JennC <2843274115LL@gmail.com>
2023-09-21 10:38:10 +0800

    完成推荐模块

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home';
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home';
 import CustomNavbar from './componnets/CustomNavbar.vue';
 import CategoryPanel from './componnets/CategoryPanel.vue';
+import HotPanel from './componnets/HotPanel.vue';
 import { onLoad } from '@dcloudio/uni-app';
 import { ref } from 'vue';
-import type { BannerItem, CategoryItem } from '@/types/home';
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home';
 // 空数组在 ts 里面没有指定类型  以后只能是空数组 不能存放任何数据
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -21,10 +22,20 @@ const getHomeCategoryData = async () => {
   categoryList.value = res.result
 }
 
+// 获取热门推荐数据
+const homeHotList = ref<HotItem[]>([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  // console.log('res', res);
+  homeHotList.value = res.result
+
+}
+
 // 页面加载的时候调用一次
 onLoad(() => {
   getHomeBannerData()
   getHomeCategoryData()
+  getHomeHotData()
 })
 </script>
 
@@ -35,6 +46,8 @@ onLoad(() => {
   <XtxSwiper :list="bannerList" />
   <!-- 分类面板 -->
   <CategoryPanel :list="categoryList" />
+  <!-- 热门推荐 -->
+  <HotPanel :list="homeHotList" />
   <view class="index">index</view>
 </template>
 
