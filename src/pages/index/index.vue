75fbdc5f92d6dca22ed2c6d04fc565cfa9f9a757
JennC <2843274115LL@gmail.com>
2023-09-21 09:24:49 +0800

    轮播图API接口请求，轮播图类型定义和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '@/services/home';
 import CustomNavbar from './componnets/CustomNavbar.vue';
+import { onLoad } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+import type { BannerItem } from '@/types/home';
+// 空数组在 ts 里面没有指定类型  以后只能是空数组 不能存放任何数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  // console.log(res.result);
+  bannerList.value = res.result
+
+}
+// 页面加载的时候调用一次
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
   <!-- 自定义轮播图 -->
-  <XtxSwiper />
+  <XtxSwiper :list="bannerList" />
   <view class="index">index</view>
 </template>
 
