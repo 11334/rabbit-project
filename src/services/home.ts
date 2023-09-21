99fc6976b3410f6675d71674396c00d103b8bd16
JennC <2843274115LL@gmail.com>
2023-09-21 14:03:14 +0800

    猜你喜欢类型定义和列表渲染

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,5 +1,6 @@
-import type { BannerItem, CategoryItem, HotItem } from '@/types/home';
+import type { BannerItem, CategoryItem, GuessItem, HotItem } from '@/types/home';
 import { http } from './../utils/http';
+import type { PageResult } from '@/types/global';
 
 /**
  * 首页-广告区域-小程序
@@ -43,7 +44,7 @@ export const getHomeHotAPI = () => {
  * 猜你喜欢-小程序
  */
 export const getHomeGoodsGuessLikeAPI = () => {
-  return http({
+  return http<PageResult<GuessItem>>({
     method: 'GET',
     url: '/home/goods/guessLike',
   })
