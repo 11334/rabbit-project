34c1968f87ca37ebfbce570cab5476d98f4474e1
JennC <2843274115LL@gmail.com>
2023-09-28 13:49:35 +0800

    弹出层组件结构

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -40,6 +40,12 @@ const onTapImage = (url: string) => {
     urls: goods.value!.mainPictures
   })
 }
+
+// uni-ui 弹出层组件  ref  组件实例
+const popup = ref<{
+  open: (type?: UniHelper.UniPopupType) => void
+  close: () => void
+}>()
 </script>
 
 <template>
@@ -80,7 +86,7 @@ const onTapImage = (url: string) => {
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view class="item arrow">
+        <view @tap="popup?.open('bottom')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -140,6 +146,13 @@ const onTapImage = (url: string) => {
       <view class="buynow"> 立即购买 </view>
     </view>
   </view>
+
+  <!-- uni-ui 弹出层 -->
+  <uni-popup ref="popup" type="bottom" background-color="#fff">
+    <view>内容1</view>
+    <view>内容2</view>
+    <button @tap="popup?.close">关闭弹出层</button>
+  </uni-popup>
 </template>
 
 <style lang="scss">
