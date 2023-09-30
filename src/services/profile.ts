e8cba2d5ff177107272f419e2092211ae8b15266
JennC <2843274115LL@gmail.com>
2023-09-30 23:29:32 +0800

    会员中心-修改用户昵称

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,4 +1,4 @@
-import type { ProfileDetail } from "@/types/member"
+import type { ProfileDetail, ProfileParams } from "@/types/member"
 import { http } from "@/utils/http"
 
 /**
@@ -13,6 +13,14 @@ export const getMemberProfileAPI = () => {
 
 
 /**
- * 修改个人信息
+ * 修改个人信息 
  * @param data 请求体参数
  */
+
+export const putMemberProfileAPI = (data: ProfileParams) => {
+  return http({
+    method: "PUT",
+    url: '/member/profile',
+    data
+  })
+}
